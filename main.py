414f7cb857b8c07dd18051f35a20e4e2d5fe3968
warmer_autumn <osharebones@protonmail.com>
2022-01-29 13:32:00 -0600

    got reactions working on images

--- main.py
+++ main.py
@@ -11,18 +11,22 @@ load_dotenv('test.env')
 my_secret = os.getenv('TRAIN')
 
 bot = hikari.GatewayBot(token=my_secret)
-
+#test = hikari.KnownCustomEmoji
 
 @bot.listen()
-async def ping(event: hikari.GuildMessageCreateEvent) -> None:
+async def ping(event: hikari.GuildMessageCreateEvent):
     # If a non-bot user sends a message "hk.ping", respond with "Pong!"
     # We check there is actually content first, if no message content exists,
     # we would get `None' here.
     if event.is_bot or not event.content:
         return
-
     if event.content.startswith("choo choo"):
           await event.message.respond(":steam_locomotive::railway_car::railway_car::railway_car::transgender_flag::transgender_flag::transgender_flag::railway_car::railway_car::railway_car:")
-  
+    if event.message.attachments:
+        #print(test)
+        await event.message.add_reaction("dept_of_trains", 920417740911702036)
+        
+#async def ping(event: hikari.GuildMessageCreateEvent):
+    
 #print(os.environ)
 bot.run()
